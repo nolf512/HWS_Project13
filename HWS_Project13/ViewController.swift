9874d99caed254654b627253e5c620c7e2e4c3fe
nofprog <k.jin3217@gmail.com>
2021-04-14 23:36:28 +0900

    UIImagePickerController 追加

--- HWS_Project13/ViewController.swift
+++ HWS_Project13/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    
+    var currentImage: UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "CoreImage"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importImage))
+        
     }
-
-
+    
+    @objc func importImage(){
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true, completion: nil)
+        
+        currentImage = image
+        
+        
+        
+    }
+    
+    
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    
+    
+    @IBAction func intensityChange(_ sender: Any) {
+    }
+    
+    
+    
 }
 
